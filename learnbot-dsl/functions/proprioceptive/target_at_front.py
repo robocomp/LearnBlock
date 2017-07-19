56eae5b1ffe8f1b9cbddf229b806ab9046f458db
pilarbachiller <pilar.bachiller@gmail.com>
2017-07-19 17:38:05 +0200

    Correction in learnBotDSL

--- learnbot-dsl/functions/proprioceptive/target_at_front.py
+++ learnbot-dsl/functions/proprioceptive/target_at_front.py
@@ -1,14 +1,13 @@
 from __future__ import print_function
 import math as m
 
-def target_at_front(lbot, targetX, targetY, maxFrontAngle = 0.2, verbose=False):
+def target_at_front(lbot, targetX, targetY, diffTargetX = 20, verbose=False):
 	x, y, alpha = lbot.getPose()
 	targetFromRobotX = m.cos(alpha)*(targetX-x) - m.sin(alpha)*(targetY-y)
 	targetFromRobotY = m.sin(alpha)*(targetX-x) + m.cos(alpha)*(targetY-y)
-	frontAngle = m.atan2(targetFromRobotX, targetFromRobotY)
 	if verbose:
 		print("Target from robot", targetFromRobotX, targetFromRobotY)
-	if frontAngle<=maxFrontAngle:
+	if targetFromRobotY>=0 and m.fabs(targetFromRobotX)<=diffTargetX:
 		return True
 	return False
 
--- learnbot-dsl/go_to_target_example.py
+++ learnbot-dsl/go_to_target_example.py
@@ -9,10 +9,34 @@ from functions import *
 global lbot
 lbot = LearnBotClient.Client(sys.argv)
 
+targetX = 0
+targetY = 1800
 
-while True:
+while not functions.get("near_to_target")(lbot, targetX, targetY):
 
 	functions.get("get_pose")(lbot, True)
-	if functions.get("target_at_front")(lbot, -100, 500, 0.2, True):
-		print "Target at front"
 
+	if functions.get("front_obstacle")(lbot):
+		while functions.get("front_obstacle")(lbot) or not functions.get("left_obstacle")(lbot, 400):
+			functions.get("turn_right")(lbot)
+		print "front obstacle"
+	else:
+		if functions.get("target_at_front")(lbot, targetX, targetY):
+			functions.get("move_straight")(lbot)
+			print "going to target"
+		elif functions.get("target_at_right")(lbot, targetX, targetY):
+			print "target at right"
+			if functions.get("right_obstacle")(lbot, 400):
+				functions.get("move_left")(lbot)
+			else:
+				functions.get("turn_right")(lbot)
+		else:
+
+			if functions.get("left_obstacle")(lbot, 400):
+				print "target at left: move straight"
+				functions.get("move_right")(lbot)
+			else:
+				print "target at left: turn left"
+				functions.get("turn_left")(lbot)
+
+functions.get("stop_bot")(lbot)
