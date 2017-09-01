bb2505299205e683746fa2874fb4bc974d82ad11
pilar <pilar.bachiller@gmail.com>
2017-09-01 13:19:24 +0200

    change of thickness in putText

--- learnbot_dsl/learnbotCode/guis/var.py
+++ learnbot_dsl/learnbotCode/guis/var.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '/home/ivan/robocomp/components/learnbot/learnbot_dsl/learnbotCode/guis/var.ui'
+# Form implementation generated from reading ui file '/home/pilar/robocomp/components/learnbot/learnbot_dsl/learnbotCode/guis/var.ui'
 #
-# Created: Wed Aug 30 19:10:13 2017
+# Created: Fri Sep  1 12:59:18 2017
 #      by: pyside-uic 0.2.15 running on PySide 1.2.2
 #
 # WARNING! All changes made in this file will be lost!
