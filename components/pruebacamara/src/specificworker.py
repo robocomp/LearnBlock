4fe024bf55693122a140d964b8956536dcf7a5c3
ibarbech <ibarbech@alumnos.unex.es>
2018-01-17 13:11:18 +0100

    add component pruebacamara

---
+++ components/pruebacamara/src/specificworker.py
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2018 by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys, os, traceback, time
+
+from PySide import QtGui, QtCore
+from genericworker import *
+import cv2
+import numpy as np
+import time
+
+# If RoboComp was compiled with Python bindings you can use InnerModel in Python
+# sys.path.append('/opt/robocomp/lib')
+# import librobocomp_qmat
+# import librobocomp_osgviewer
+# import librobocomp_innermodel
+
+class SpecificWorker(GenericWorker):
+	def __init__(self, proxy_map):
+		super(SpecificWorker, self).__init__(proxy_map)
+		self.timer.timeout.connect(self.compute)
+		self.Period = 0
+		self.timer.start(self.Period)
+
+	def setParams(self, params):
+		#try:
+		#	self.innermodel = InnerModel(params["InnerModelPath"])
+		#except:
+		#	traceback.print_exc()
+		#	print "Error reading config params"
+		return True
+
+	@QtCore.Slot()
+	def compute(self):
+		start = time.time()
+		color, depth, headState, baseState = self.rgbd_proxy.getData()
+		img = np.fromstring(color, dtype = np.uint8).reshape((240, 320, 3))
+		cv2.imshow('messigray.png',img)
+		end = time.time()
+		seconds = end - start
+		fps  = 1 / seconds;
+		print "Estimated frames per second : {0}".format(fps);
+		return True
