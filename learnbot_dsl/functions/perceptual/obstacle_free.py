e0af147e7b8ab58add721fefe425c6ac28183043
ibarbech <ibarbech@alumnos.unex.es>
2018-04-25 13:57:24 +0200

    update function obstracle free and solved bug from xml learnbot_simulator/version_2.1/learnBotWorldDSL_lines2.xml

--- learnbot_dsl/functions/perceptual/obstacle_free.py
+++ learnbot_dsl/functions/perceptual/obstacle_free.py
@@ -1,6 +1,6 @@
 def obstacle_free(lbot, threshold= 200, verbose=False):
 	sonarsValue = lbot.getSonars()
-	if min(sonarsValue) > threshold:
+	if min(sonarsValue) < threshold:
 		if verbose:
 			print('No obstacles around Learnbot')
 		return True
