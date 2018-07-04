1f3210967eb429e089e63c1dd396eccc287c58e8
ibarbech <ibarbech@alumnos.unex.es>
2018-07-04 12:15:14 +0000

    update laser and startComponente

--- components/laser/src/specificworker.py
+++ components/laser/src/specificworker.py
@@ -21,7 +21,7 @@ import sys, os, traceback, time
 
 from PySide import QtGui, QtCore
 from genericworker import *
-
+import VL53L0X, time
 
 # If RoboComp was compiled with Python bindings you can use InnerModel in Python
 # sys.path.append('/opt/robocomp/lib')
@@ -34,7 +34,14 @@ class SpecificWorker(GenericWorker):
         super(SpecificWorker, self).__init__(proxy_map)
         self.timer.timeout.connect(self.compute)
         self.Period = 2000
-        self.timer.start(self.Period)
+        self.tof = VL53L0X.VL53L0X()
+        self.tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+        self.timing = self.tof.get_timing()
+        if (self.timing < 20000):
+            self.timing = 20000
+        print ("Timing %d ms" % (self.timing/1000))
+
+        self.timer.start(self.timing/1000)
 
     def setParams(self, params):
         # try:
@@ -46,28 +53,18 @@ class SpecificWorker(GenericWorker):
 
     @QtCore.Slot()
     def compute(self):
-        print ('SpecificWorker.compute...')
-        # computeCODE
-        # try:
-        #	self.differentialrobot_proxy.setSpeedBase(100, 0)
-        # except Ice.Exception, e:
-        #	traceback.print_exc()
-        #	print e
-
-        # The API of python-innermodel is not exactly the same as the C++ version
-        # self.innermodel.updateTransformValues("head_rot_tilt_pose", 0, 0, 0, 1.3, 0, 0)
-        # z = librobocomp_qmat.QVec(3,0)
-        # r = self.innermodel.transform("rgbd", z, "laser")
-        # r.printvector("d")
-        # print r[0], r[1], r[2]
-
+        distance = self.tof.get_distance()
+        data = TData()
+        data.dist = distance
+        data.angle = 0
+        self.laserData=[data, data, data, data, data]
         return True
 
     #
     # getLaserData
     #
     def getLaserData(self):
-        ret = TLaserData()
+        ret = self.laserData
         #
         # implementCODE
         #
