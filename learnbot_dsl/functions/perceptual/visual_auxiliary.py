004a4d471a56b3b223b2d904c8e0ba12d73f85b9
ibarbech <ibarbech@alumnos.unex.es>
2018-09-18 13:44:49 +0200

    fix problem with visual auxiliary

--- learnbot_dsl/functions/perceptual/visual_auxiliary.py
+++ learnbot_dsl/functions/perceptual/visual_auxiliary.py
@@ -2,8 +2,11 @@ from __future__ import print_function
 import cv2
 import numpy as np
 
-LL_red = (0, 70, 50)
+LL_red = (0, 50, 100)
 LU_red = (10, 255, 255)
+
+LL_red2 = (170, 50, 100)
+LU_red2 = (180, 255, 255)
 
 LL_blue = (100, 50, 50)
 LU_blue = (130, 255, 255)
@@ -15,19 +18,20 @@ def detect_black_line(frame):
     for channel in rgb:
         maxrgb = cv2.max(maxrgb, channel)
 
-    err, binary = cv2.threshold(maxrgb, 100, 255, cv2.THRESH_BINARY_INV)
+    err, binary = cv2.threshold(maxrgb, 50, 255, cv2.THRESH_BINARY_INV)
 
     rois = [0, 0, 0]
-    rois[0] = cv2.countNonZero(binary[10:240, 0:120])
-    rois[1] = cv2.countNonZero(binary[10:240, 120:200])
-    rois[2] = cv2.countNonZero(binary[10:240, 200:320])
+    rois[0] = cv2.countNonZero(binary[15:240, 0:120])
+    rois[1] = cv2.countNonZero(binary[15:240, 120:200])
+    rois[2] = cv2.countNonZero(binary[15:240, 200:320])
     return rois
 
 
 def detect_red_line(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
     binary = cv2.inRange(hsv, LL_red, LU_red)
-
+    binary2 = cv2.inRange(hsv, LL_red2, LU_red2)
+    binary = binary + binary2
     err, binary = cv2.threshold(binary, 100, 255, cv2.THRESH_BINARY)
     binary = cv2.dilate(binary, None, iterations=3)
 
