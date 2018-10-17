48c22cfb895f4d40250b9186507a9374e64ebe1e
ibarbech <ibarbech@alumnos.unex.es>
2018-10-17 22:58:19 +0200

    update for pip install learnbot-dsl

--- learnbot_dsl/__init__.py
+++ learnbot_dsl/__init__.py
@@ -2,4 +2,5 @@ import learnbot_dsl.LearnBotClient as LearnBotClient
 import learnbot_dsl.functions as functions
 import learnbot_dsl.LearnBotClient_PhysicalRobot as LearnBotClient_PhysicalRobot
 
+name = "learnbot_dsl"
 __all__ = ['LearnBotClient','LearnBotClient_PhysicalRobot', 'functions']
