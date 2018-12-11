1315480bb5e891f108498a4ea0de10f93b3768f0
ibarbech <ibarbech@alumnos.unex.es>
2018-12-11 21:24:34 +0100

    setup only plataform Posix add file .travis.yml

--- learnbot_dsl_setup.py
+++ learnbot_dsl_setup.py
@@ -57,6 +57,7 @@ setup(name="learnbot_dsl",
       long_description=read('learnbot_dsl/description.md'),
       long_description_content_type='text/markdown',
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+      platforms="Posix",
       install_requires=[
           "requests>=2.20.0",
           "pyunpack>=0.1.2",
