458eca10691dae19b10db3fead89d8864237a21d
pilarbachiller <pilar.bachiller@gmail.com>
2019-05-29 13:36:01 +0200

    EV3 base working

--- learnbot_dsl/Clients/EV3.py
+++ learnbot_dsl/Clients/EV3.py
@@ -7,14 +7,17 @@ import rpyc
 
 K = 30
 L = 120
+MAXSPEED = 1000
 
 class Robot(Client):
 
     devicesAvailables = ["base"]
 
     def __init__(self):
+        self.ev3 = None
+        self.motorR = None
+        self.motorL = None
         self.connectToRobot()
-        self.deviceBaseMove(0,0)
         self.base = Base(_callFunction=self.deviceBaseMove)
         self.motorSpeed = [0, 0]
         self.currentMotorSpeed = [-1, -1]
@@ -22,30 +25,32 @@ class Robot(Client):
         self.start()
 
     def connectToRobot(self):
-        conn = rpyc.classic.connect('192.168.0.119')  # host name or IP address of the EV3
-        self.ev3 = conn.modules['ev3dev.ev3']  # import ev3dev.ev3 remotely
+        self. conn = rpyc.classic.connect('192.168.0.119')  # host name or IP address of the EV3
+        self.ev3 = self.conn.modules['ev3dev.ev3']  # import ev3dev.ev3 remotely
         self.motorR = self.ev3.LargeMotor('outD')
         self.motorL = self.ev3.LargeMotor('outB')
 
 
     def disconnect(self):
-        self.deviceBaseMove(0, 0)
+        self.motorR.run_timed(time_sp=100, speed_sp=0)
+        self.motorL.run_timed(time_sp=100, speed_sp=0)
+
 
     def deviceBaseMove(self, SAdv, SRot):
         if SRot != 0.:
             Rrot = SAdv / math.tan(SRot)
 
             Rl = Rrot - (L / 2)
-            r_wheel_speed = SRot * Rl / K
+            r_wheel_speed = SRot * Rl * 360/ (2 * math.pi * K)
 
             Rr = Rrot + (L / 2)
-            l_wheel_speed = SRot * Rr / K
+            l_wheel_speed = SRot * Rr * 360/ (2 * math.pi * K)
         else:
-            l_wheel_speed = SAdv / K
-            r_wheel_speed = SAdv / K
-        self.motorR.run_forever(speed_sp = r_wheel_speed)
-        self.motorL.run_forever(speed_sp = l_wheel_speed)
-
+            l_wheel_speed = SAdv * 360/ (2 * math.pi * K)
+            r_wheel_speed = SAdv * 360/ (2 * math.pi * K)
+        print("rspeed", r_wheel_speed, "lspeed", l_wheel_speed)
+        self.motorR.run_forever(speed_sp = int(r_wheel_speed))
+        self.motorL.run_forever(speed_sp = int(l_wheel_speed))
 
 
 if __name__ == '__main__':
