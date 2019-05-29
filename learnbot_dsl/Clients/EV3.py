0c5f77ea15c4bc642934f0632c9fca0c37fcaa7c
pilarbachiller <pilar.bachiller@gmail.com>
2019-05-29 10:46:21 +0200

    EV3 client update

--- learnbot_dsl/Clients/EV3.py
+++ learnbot_dsl/Clients/EV3.py
@@ -3,7 +3,10 @@ from learnbot_dsl.Clients.Devices import *
 from learnbot_dsl.functions import getFuntions
 import math, traceback, sys, tempfile, os
 from threading import Event
+import rpyc
 
+K = 30
+L = 120
 
 class Robot(Client):
 
@@ -19,21 +22,30 @@ class Robot(Client):
         self.start()
 
     def connectToRobot(self):
+        conn = rpyc.classic.connect('192.168.0.119')  # host name or IP address of the EV3
+        self.ev3 = conn.modules['ev3dev.ev3']  # import ev3dev.ev3 remotely
+        self.motorR = self.ev3.LargeMotor('outD')
+        self.motorL = self.ev3.LargeMotor('outB')
 
+
+    def disconnect(self):
+        self.deviceBaseMove(0, 0)
 
     def deviceBaseMove(self, SAdv, SRot):
         if SRot != 0.:
             Rrot = SAdv / math.tan(SRot)
 
             Rl = Rrot - (L / 2)
-            r_wheel_speed = SRot * Rl * K
+            r_wheel_speed = SRot * Rl / K
 
             Rr = Rrot + (L / 2)
-            l_wheel_speed = SRot * Rr * K
+            l_wheel_speed = SRot * Rr / K
         else:
-            l_wheel_speed = SAdv * K
-            r_wheel_speed = SAdv * K
-        self.motorSpeed = [l_wheel_speed, r_wheel_speed]
+            l_wheel_speed = SAdv / K
+            r_wheel_speed = SAdv / K
+        self.motorR.run_forever(speed_sp = r_wheel_speed)
+        self.motorL.run_forever(speed_sp = l_wheel_speed)
+
 
 
 if __name__ == '__main__':
