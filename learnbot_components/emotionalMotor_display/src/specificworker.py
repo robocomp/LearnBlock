e1b487f2ebebd06a921fd34eb7b1365ef852aa7c
pilarbachiller <pilar.bachiller@gmail.com>
2019-09-16 18:13:27 +0200

    new component emotionalMotor using the component display

---
+++ learnbot_components/emotionalMotor_display/src/specificworker.py
@@ -0,0 +1,286 @@
+#
+# Copyright (C) 2017 by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys, os, traceback, time, copy, json
+
+from PySide import QtGui, QtCore
+from learnbot_components.emotionalMotor_display.src.genericworker import *
+from learnbot_dsl import path as emopath
+from PIL import Image, ImageDraw
+import cv2, numpy as np
+# If RoboComp was compiled with Python bindings you can use InnerModel in Python
+# sys.path.append('/opt/robocomp/lib')
+# import librobocomp_qmat
+# import librobocomp_osgviewer
+# import librobocomp_innermodel
+configPath = os.path.join(os.path.dirname(os.path.dirname(__file__)),'etc','config')
+#DEFAULTCONFIGNEUTRAL = {'cejaD': {'P2': {'y': 73, 'x': 314}, 'P3': {'y': 99, 'x': 355}, 'P1': {'y': 99, 'x': 278}, 'P4': {'y': 94, 'x': 313}}, 'ojoI': {'P2': {'y': 186, 'x': 196}, 'P1': {'y': 117, 'x': 127}}, 'cejaI': {'P2': {'y': 73, 'x': 160}, 'P3': {'y': 99, 'x': 201}, 'P1': {'y': 99, 'x': 122}, 'P4': {'y': 94, 'x': 160}}, 'ojoD': {'P2': {'y': 186, 'x': 351}, 'P1': {'y': 117, 'x': 282}}, 'boca': {'P2': {'y': 231, 'x': 239}, 'P3': {'y': 234, 'x': 309}, 'P1': {'y': 234, 'x': 170}, 'P6': {'y': 242, 'x': 170}, 'P4': {'y': 242, 'x': 309}, 'P5': {'y': 241, 'x': 239}}}
+DEFAULTCONFIGNEUTRAL = {
+    "cejaD": {"P2": {"y": 73, "x": 314},
+                                  "P3": {"y": 99, "x": 355},
+                                  "P1": {"y": 99, "x": 278},
+                                  "P4": {"y": 94, "x": 313}
+                                  },
+                        "parpadoI": {"P2": {"y": 80, "x": 160},
+                                     "P3": {"y": 151, "x": 214},
+                                     "P1": {"y": 151, "x": 112},
+                                     "P4": {"y": 80, "x": 160}},
+                        "ojoI": {"Radio1": {"Value": 34},
+                                 "Center": {"y": 151, "x": 161},
+                                 "Radio2": {"Value": 34}},
+                        "cejaI": {"P2": {"y": 73, "x": 160},
+                                  "P3": {"y": 99, "x": 201},
+                                  "P1": {"y": 99, "x": 122},
+                                  "P4": {"y": 94, "x": 160}},
+                        "ojoD": {"Radio1": {"Value": 34},
+                                 "Center": {"y": 151, "x": 316},
+                                 "Radio2": {"Value": 34}},
+                        "boca": {"P2": {"y": 231, "x": 239},
+                                 "P3": {"y": 234, "x": 309},
+                                 "P1": {"y": 234, "x": 170},
+                                 "P6": {"y": 242, "x": 170},
+                                 "P4": {"y": 242, "x": 309},
+                                 "P5": {"y": 241, "x": 239}},
+                        "pupilaD": {"Radio": {"Value": 5},
+                                    "Center": {"y": 151, "x": 316}},
+                        "lengua": {"P2": {"y": 238, "x": 239},
+                                   "P3": {"y": 238, "x": 309},
+                                   "P1": {"y": 238, "x": 199},
+                                   "P4": {"y": 238, "x": 273}},
+                        "mejillaI": {"P2": {"y": 188, "x": 160},
+                                     "P3": {"y": 187, "x": 201},
+                                     "P1": {"y": 187, "x": 122},
+                                     "P4": {"y": 187, "x": 160}},
+                        "parpadoD": {"P2": {"y": 80, "x": 314},
+                                     "P3": {"y": 151, "x": 369},
+                                     "P1": {"y": 151, "x": 266},
+                                     "P4": {"y": 80, "x": 313}},
+                        "pupilaI": {"Radio": {"Value": 5},
+                                    "Center": {"y": 151, "x": 161}},
+                        "mejillaD": {"P2": {"y": 188, "x": 314},
+                                     "P3": {"y": 187, "x": 355},
+                                     "P1": {"y": 187, "x": 278},
+                                     "P4": {"y": 187, "x": 313}}}
+
+OFFSET = 0.06666666666666667
+def bezier(p1, p2, t):
+	diff = (p2[0] - p1[0], p2[1] - p1[1])
+	return [p1[0] + diff[0] * t, p1[1] + diff[1] * t]
+
+def getPointsBezier(points):
+	bezierPoints = list()
+	pointsCopy = copy.copy(points)
+	for t in [x/50. for x in range(51)]:
+		while len(points)!=1:
+			newPoints = list()
+			p1=points[0]
+			for p2 in points[1:]:
+				newPoints.append(bezier(p1,p2,t))
+				p1=p2
+			points=newPoints
+		bezierPoints.append(tuple(points[0]))
+		points=pointsCopy
+	return bezierPoints
+
+def getBecierConfig(old_config, config_target, t):
+	config = copy.copy(old_config)
+	for parte in old_config:
+		for point in old_config[parte]:
+			if "Radio" in point:
+				radio = bezier((old_config[parte][point]["Value"],0), (config_target[parte][point]["Value"],0),t)
+				config[parte][point]["Value"] = radio[0]
+			else:
+				p = bezier((old_config[parte][point]["x"], old_config[parte][point]["y"]), (config_target[parte][point]["x"], config_target[parte][point]["y"]), t)
+				config[parte][point]["x"] = p[0]
+				config[parte][point]["y"] = p[1]
+	return config
+
+class Face():
+
+	def __init__(self):
+		self.img = Image.new('RGB', (480, 320), (255, 255, 255))
+		self.draw = ImageDraw.Draw(self.img)
+		self.config = DEFAULTCONFIGNEUTRAL
+		self.old_config = DEFAULTCONFIGNEUTRAL
+		self.t = 0.06666666666666667
+		self.config_target = None
+
+	def render(self):
+		if self.t <= 1 and self.config_target is not None:
+			self.config = getBecierConfig(self.old_config, self.config_target, self.t)
+			self.draw.rectangle(((0, 0), (479, 319)), fill=(255, 255, 255), outline=(255, 255, 255))
+			self.renderOjo(self.config["ojoI"])
+			self.renderOjo(self.config["ojoD"])
+			self.renderParpado(self.config["parpadoI"])
+			self.renderParpado(self.config["parpadoD"])
+			self.renderCeja(self.config["cejaI"])
+			self.renderCeja(self.config["cejaD"])
+			self.renderBoca(self.config["boca"])
+			self.renderPupila(self.config["pupilaI"])
+			self.renderPupila(self.config["pupilaD"])
+			self.renderMejilla(self.config["mejillaI"])
+			self.renderMejilla(self.config["mejillaD"])
+			self.t += OFFSET
+			#path = "/dev/fb0"
+			#with open(path, "wb") as f:
+				#f.write(self.img.tobytes())
+
+			np.array(self.img)
+			cv2.imwrite("/tmp/ebofaceimg.png",np.array(self.img))
+			path = "/tmp/ebofaceimg.png"
+			return path
+		else:
+			self.old_config = self.config_target
+			self.config_target = None
+		return None
+
+	def renderPupila(self, points):
+		P1 = (points["Center"]["x"] - points["Radio"]["Value"], points["Center"]["y"] - points["Radio"]["Value"])
+		P2 = (points["Center"]["x"] + points["Radio"]["Value"], points["Center"]["y"] + points["Radio"]["Value"])
+		self.draw.ellipse((P1, P2), fill=(255,255,255), outline=(255,255,255))
+		# self.draw.ellipse((P1, P2), fill=1)
+
+	def renderParpado(self, points):
+		P1 = (points["P1"]["x"], points["P1"]["y"])
+		P2 = (points["P2"]["x"], points["P2"]["y"])
+		P3 = (points["P3"]["x"], points["P3"]["y"])
+		P4 = (points["P4"]["x"], points["P4"]["y"])
+		self.draw.polygon(getPointsBezier([P1,P2,P3]) + getPointsBezier([P3,P4,P1]), fill=(255,255,255))
+
+	def renderMejilla(self, points):
+		P1 = (points["P1"]["x"], points["P1"]["y"])
+		P2 = (points["P2"]["x"], points["P2"]["y"])
+		P3 = (points["P3"]["x"], points["P3"]["y"])
+		P4 = (points["P4"]["x"], points["P4"]["y"])
+		self.draw.polygon(getPointsBezier([P1,P2,P3]) + getPointsBezier([P3,P4,P1]), fill=(255,255,255))
+
+	def renderCeja(self, points):
+		P1 = (points["P1"]["x"], points["P1"]["y"])
+		P2 = (points["P2"]["x"], points["P2"]["y"])
+		P3 = (points["P3"]["x"], points["P3"]["y"])
+		P4 = (points["P4"]["x"], points["P4"]["y"])
+		self.draw.polygon(getPointsBezier([P1, P2, P3]) + getPointsBezier([P3, P4, P1]), fill=1)
+
+	def renderOjo(self, points):
+		P1 = (points["Center"]["x"] - points["Radio1"]["Value"], points["Center"]["y"] - points["Radio2"]["Value"])
+		P2 = (points["Center"]["x"] + points["Radio1"]["Value"], points["Center"]["y"] + points["Radio2"]["Value"])
+		# P1 = (points["P1"]["x"], points["P1"]["y"])
+		# P2 = (points["P2"]["x"], points["P2"]["y"])
+		self.draw.ellipse((P1, P2), fill=1)
+
+	def renderBoca(self, points):
+		P1 = (points["P1"]["x"], points["P1"]["y"])
+		P2 = (points["P2"]["x"], points["P2"]["y"])
+		P3 = (points["P3"]["x"], points["P3"]["y"])
+		P4 = (points["P4"]["x"], points["P4"]["y"])
+		P5 = (points["P5"]["x"], points["P5"]["y"])
+		P6 = (points["P6"]["x"], points["P6"]["y"])
+		self.draw.polygon(getPointsBezier([P1, P2, P3]) + getPointsBezier([P4, P5, P6]), fill=1, outline=10)
+
+	def setConfig(self, config):
+		self.config_target = config
+		self.old_config = self.config
+		self.t = 0.06666666666666667
+
+
+class SpecificWorker(GenericWorker):
+	def __init__(self, proxy_map):
+		print("specificworker created")
+		super(SpecificWorker, self).__init__(proxy_map)
+		self.timer.timeout.connect(self.compute)
+		self.Period = 66
+		self.timer.start(self.Period)
+		self.face = Face()
+		self.configEmotions = {}
+		for path in os.listdir(os.path.join(emopath, "imgs/emotionConfig")):
+			if os.path.splitext(path)[1] == ".json":
+				with open(os.path.join(emopath, "imgs/emotionConfig", path), "r") as f:
+					self.configEmotions[os.path.splitext(path)[0]] = json.loads(f.read())
+		self.expressNeutral()
+
+	def setParams(self, params):
+		return True
+
+	@QtCore.Slot()
+	def compute(self):
+		path = self.face.render()
+		if path is not None:
+			self.display_proxy.setImageFromFile(path)
+		return True
+
+	def sendImage(self, file):
+		img = QtGui.QImage(file)
+		self.i+=1
+		im = Image()
+		im.Img=img.bits()
+		im.width=img.width()
+		im.height=img.height()
+		try:
+			self.display_proxy.setImage(im)
+		except Exception as e:
+			traceback.print_exc()
+
+	#
+	# expressFear
+	#
+	def expressFear(self):
+		self.face.setConfig(self.configEmotions["Fear"])
+		# self.display_proxy.setImageFromFile("/home/robocomp/learnbot/learnbot_components/emotionalMotor/imgs/frameBuffer/miedo.fb")
+
+	#
+	# expressSurprise
+	#
+	def expressSurprise(self):
+		self.face.setConfig(self.configEmotions["Surprise"])
+		# self.display_proxy.setImageFromFile("/home/robocomp/learnbot/learnbot_components/emotionalMotor/imgs/frameBuffer/sorpresa.fb")
+
+	#
+	# expressAnger
+	#
+	def expressAnger(self):
+		self.face.setConfig(self.configEmotions["Anger"])
+		# self.display_proxy.setImageFromFile("/home/robocomp/learnbot/learnbot_components/emotionalMotor/imgs/frameBuffer/ira.fb")
+
+	#
+	# expressSadness
+	#
+	def expressSadness(self):
+		self.face.setConfig(self.configEmotions["Sadness"])
+		# self.display_proxy.setImageFromFile("/home/robocomp/learnbot/learnbot_components/emotionalMotor/imgs/frameBuffer/tristeza.fb")
+
+	#
+	# expressDisgust
+	#
+	def expressDisgust(self):
+		self.face.setConfig(self.configEmotions["Disgust"])
+		# self.display_proxy.setImageFromFile("/home/robocomp/learnbot/learnbot_components/emotionalMotor/imgs/frameBuffer/asco.fb")
+
+	#
+	# expressJoy
+	#
+	def expressJoy(self):
+		self.face.setConfig(self.configEmotions["Joy"])
+		# self.display_proxy.setImageFromFile("/home/robocomp/learnbot/learnbot_components/emotionalMotor/imgs/frameBuffer/alegria.fb")
+
+	#
+	# expressNeutral
+	#
+	def expressNeutral(self):
+		self.face.setConfig(self.configEmotions["Neutral"])
+		# self.display_proxy.setImageFromFile("/home/robocomp/learnbot/learnbot_components/emotionalMotor/imgs/frameBuffer/SinEmocion2.fb")
---
+++ learnbot_dsl/Clients/EBO_sim.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from learnbot_dsl.Clients.Client import *
+from learnbot_dsl.Clients.Devices import *
+import os, Ice, numpy as np, io, cv2, threading, json, math
+import learnbot_dsl.Clients.Devices as Devices
+from PIL import Image, ImageDraw
+from random import randint
+from learnbot_dsl import path as Learnblock_Path
+from learnbot_dsl import PATHINTERFACES
+
+
+
+ROBOCOMP = ''
+try:
+    ROBOCOMP = os.environ['ROBOCOMP']
+except KeyError:
+    print('$ROBOCOMP environment variable not set, using the default value /opt/robocomp')
+    ROBOCOMP = os.path.join('opt', 'robocomp')
+
+ICEs = ["Laser.ice", "DifferentialRobot.ice", "JointMotor.ice", "EmotionalMotor.ice", "RGBD.ice", "GenericBase.ice"]
+icePaths = []
+icePaths.append(PATHINTERFACES)
+for ice in ICEs:
+    for p in icePaths:
+        if os.path.isfile(os.path.join(p, ice)):
+            wholeStr = ' -I' + p + " --all " + os.path.join(p, ice)
+            Ice.loadSlice(wholeStr)
+            break
+
+import RoboCompLaser, RoboCompDifferentialRobot, RoboCompJointMotor, RoboCompGenericBase, RoboCompEmotionalMotor, RoboCompRGBD
+
+
+class Robot(Client):
+
+    devicesAvailables = ["base", "camera", "display", "distancesensors", "jointmotor"]
+
+    def __init__(self):
+        self.connectToRobot()
+        Client.__init__(self)
+
+        self.open_cv_image = np.zeros((240, 320, 3), np.uint8)
+        self.newImage = False
+        self.addDistanceSensors(Devices.DistanceSensors(_readFunction=self.deviceReadLaser))
+        self.addCamera(Devices.Camera(_readFunction=self.deviceReadCamera))
+        self.addBase(Devices.Base(_callFunction=self.deviceMove))
+        self.addDisplay(Devices.Display(_setEmotion=self.deviceSendEmotion, _setImage=None))
+        self.addJointMotor(Devices.JointMotor(_callDevice=self.deviceSendAngleHead, _readDevice=None), "CAMERA")
+        self.start()
+
+    def connectToRobot(self):
+        configRobot = {}
+        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "EBO_sim.cfg"), "rb") as f:
+            configRobot = json.loads(f.read())
+        self.laser_proxys = []
+        # Remote object connection for Lasers
+        robotIP = configRobot["RobotIP"]
+        self.differentialrobot_proxy = connectComponent("differentialrobot:tcp -h " + robotIP + " -p 10004",
+                                                        RoboCompDifferentialRobot.DifferentialRobotPrx)
+        self.deviceMove(0,0)
+
+        for i in range(2, 7):
+            self.laser_proxys.append(connectComponent("laser:tcp -h " + robotIP + " -p 1010" + str(i), RoboCompLaser.LaserPrx))
+
+        self.jointmotor_proxy = connectComponent("jointmotor:tcp -h " + robotIP + " -p 20000",
+                                                 RoboCompJointMotor.JointMotorPrx)
+
+        self.emotionalmotor_proxy = connectComponent("emotionalmotor:tcp -h " + robotIP + " -p 30001",
+                                                     RoboCompEmotionalMotor.EmotionalMotorPrx)
+
+        self.rgbd_proxy = connectComponent("rgbd:tcp -h " + robotIP + " -p 10097", RoboCompRGBD.RGBDPrx)
+
+    def disconnect(self):
+        self.deviceMove(0, 0)
+
+    def deviceReadLaser(self):
+        usList = []
+        for prx in self.laser_proxys:
+            laserdata = prx.getLaserData()
+            usList.append(min([x.dist for x in laserdata]))
+        #print(usList)
+        return {"front": usList[1:4],  # The values must be in mm
+                "left": usList[:2],
+                "right": usList[3:]}
+
+    def deviceMove(self, _adv, _rot):
+        self.differentialrobot_proxy.setSpeedBase(_adv, math.radians(_rot))
+
+    def deviceReadCamera(self, ):
+        color, depth, headState, baseState = self.rgbd_proxy.getData()
+        if (len(color) == 0) or (len(depth) == 0):
+            print('Error retrieving images!')
+        image = np.fromstring(color, dtype=np.uint8).reshape((240, 320, 3))
+        return image, True
+
+    def deviceSendEmotion(self, _emotion):
+        if _emotion is Emotions.Joy:
+            self.emotionalmotor_proxy.expressJoy()
+        elif _emotion is Emotions.Sadness:
+            self.emotionalmotor_proxy.expressSadness()
+        elif _emotion is Emotions.Surprise:
+            self.emotionalmotor_proxy.expressSurprise()
+        elif _emotion is Emotions.Disgust:
+            self.emotionalmotor_proxy.expressDisgust()
+        elif _emotion is Emotions.Anger:
+            self.emotionalmotor_proxy.expressAnger()
+        elif _emotion is Emotions.Fear:
+            self.emotionalmotor_proxy.expressFear()
+        elif _emotion is Emotions.Neutral:
+            self.emotionalmotor_proxy.expressNeutral()
+
+    def deviceSendAngleHead(self, _angle):
+        goal = RoboCompJointMotor.MotorGoalPosition()
+        goal.name = 'servo'
+        goal.position = -math.radians(_angle)
+        self.jointmotor_proxy.setPosition(goal)
+
+
+if __name__ == '__main__':
+    ebo = Robot()
+    ebo.start()
+    ebo.setBaseSpeed(0, 0)
+    ebo.setJointAngle("CAMERA", 30)
