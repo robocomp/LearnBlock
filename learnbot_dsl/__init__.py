7da93049314f6d4f4964ebc2afe25e05bc4d5664
pilarbachiller <pilar.bachiller@gmail.com>
2019-12-06 18:34:51 +0100

    new learnblock version

--- learnbot_dsl/__init__.py
+++ learnbot_dsl/__init__.py
@@ -4,7 +4,7 @@ import os, sys
 path = os.path.dirname(os.path.realpath(__file__))
 __version__ = '0.3.22'
 
-__LBversion__ = '3.1.1'
+__LBversion__ = '3.1.2'
 
 PATHINTERFACES = os.path.join(path, "interfaces")
 PATHCLIENT = os.path.join(path, "Clients")
